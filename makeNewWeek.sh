250a76bbe8e0bf406b7845cf2d2cfd361733f05b
Jingu Kang <truebread@naver.com>
2018-09-06 01:10:09 +0900

    shell updated

--- makeNewWeek.sh
+++ makeNewWeek.sh
@@ -13,13 +13,17 @@ then
     cd ThisWeek
     for i in {1..5}
     do 
-	mkdir $(date -v +$(expr $day_diff + $i)$day +%m-%d)
-    done
+	dirPath=$(date -v +$(expr $day_diff + $i)$day +%m-%d)
+	mkdir $dirPath
+	touch $dirPath/jingu_$dirPath.py
+   done
 else
     mkdir ThisWeek
     cd ThisWeek
     for i in {1..5}
     do 
-	mkdir $(date -v +$(expr $day_diff + $i)$day +%m-%d)
+	dirPath=$(date -v +$(expr $day_diff + $i)$day +%m-%d)
+	mkdir $dirPath
+	touch $dirPath/jingu_$dirPath.py
     done
 fi
