16c7d141e767ab1ed0ec6d7c68e256d5ed84e322
seu5022 <kdhht5022@gmail.com>
2018-09-07 10:56:51 +0900

    Add daeha_09_07
    add daeha_09_07

---
+++ ThisWeek/09-07/daeha_09-07.py
@@ -0,0 +1,26 @@
+__author__ = "daehakim"
+__email__ = "kdhht5022@gmail.com"
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    텐서플로 텐서를 list comprehension으로 확인하는 방법
+"""
+
+import tensorflow as tf
+
+sess = tf.Session()
+
+devices = sess.list_devices()
+for d in devices:
+    print(d.name)  # /job:localhost/replica:0/task:0/device:CPU:0 (MacOS)
+
+tensor1 = tf.constant([1, 2, 3, 4, 5])
+
+vals = [tensor1[k] for k in range(tensor1.get_shape().as_list()[0])]
+print('value is {}'.format(sess.run(vals[0])))
+print('value is {}'.format(sess.run(vals[1])))
+print('value is {}'.format(sess.run(vals[2])))
+print('value is {}'.format(sess.run(vals[3])))
+print('value is {}'.format(sess.run(vals[4])))
+sess.close()
