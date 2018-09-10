746c485536a287fca06daa3e76cd323ce7406116
ㅋ <zelabean@naver.com>
2018-09-10 13:01:10 +0900

    seongbin upload 09_10

---
+++ ThisWeek/09-10/seongbin_09-10.py
@@ -0,0 +1,14 @@
+Contributors = [(1, 'kbitzing', 'Inha University', 'Electronic', 'Male'),
+                (3, 'kdhht2334', 'Inha University', 'Electronic', 'Male'),
+                (2, 'khahn0213', 'Seoul University', 'Mathematics', 'Male'),
+                (4, 'zelabean', 'Inha University', 'Electronic', 'Male'),
+                (5, '114569', 'Inha University', 'Electronic', 'Female'),
+                (6, 'sseung0703', 'Inha University', 'Electronic', 'Male')]
+
+
+sorted_Contributors = sorted(Contributors)
+for idx, id, campus, major, gender in sorted_Contributors:
+    print(id)
+
+
+
