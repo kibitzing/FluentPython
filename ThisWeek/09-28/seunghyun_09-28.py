e92aef2f470fe640dbea785948da95892e517a6a
Seung-Hyun Lee <lsh910703@gmail.com>
2018-09-28 16:48:35 +0200

    Add files via upload

---
+++ ThisWeek/09-28/seunghyun_09-28.py
@@ -0,0 +1,18 @@
+DIAL_CODES = [(86, 'China'),
+              (91, 'India'),
+              (1, 'United States'),
+              (62, 'Indonesia'),
+              (55, 'Brazil'),
+              (92, 'Pakistan'),
+              (880, 'Bangladesh'),
+              (234, 'Nigeria'),
+              (7, 'Russia'),
+              (81, 'Japan'),
+              ]
+d1 = dict(DIAL_CODES)
+print('d1:', d1.keys())
+d2 = dict(sorted(DIAL_CODES))
+print('d2:', d2.keys())
+d3 = dict(sorted(DIAL_CODES, key=lambda x:x[1]))
+print('d3:', d3.keys())
+assert d1 == d2 and d2 == d3
