c0a39278544abb6d75a281e75556c9b9f87a56a6
Jingu Kang <truebread@naver.com>
2018-10-02 23:59:06 +0900

    jingu minor change

--- ThisWeek/10-02/jingu_10-02.py
+++ ThisWeek/10-02/jingu_10-02.py
@@ -4,16 +4,21 @@ created by Jingu Kang on 10-02
 
 ## 
 chineseChar = '你好吗'
-print('你好吗? ', chineseChar.encode('gb2312'))
-print('你好吗? ', chineseChar.encode('utf-8'))
-print('你好吗? ', chineseChar.encode('cp437', errors='replace'))
-
+print('你好吗 encode with gb2312: ', chineseChar.encode('gb2312')) 
+# 你好吗 encode with gb2312:  b'\xc4\xe3\xba\xc3\xc2\xf0'
+print('你好吗 encode with utf-8: ', chineseChar.encode('utf-8'))
+# 你好吗 encode with utf-8:  b'\xe4\xbd\xa0\xe5\xa5\xbd\xe5\x90\x97'
+print('你好吗 encode with cp(error="replace"): ', chineseChar.encode('cp437', errors='replace'))
+# 你好吗 encode with cp(error="replace"):  b'???'
 gbEncodedCnCh = chineseChar.encode('gb2312')
 utfEncodedCnCh = chineseChar.encode('utf-8')
 
-print('你好吗? gb decoded -> utf decode: ', gbEncodedCnCh.decode('utf-8', errors='replace'))
-print('你好吗? gb decoded -> gb decode: ', gbEncodedCnCh.decode('gb2312', errors='replace'))
-print('你好吗? utf encoded -> gb decode: ', utfEncodedCnCh.decode('gb2312', errors='replace'))
+print('你好吗 gb decoded -> utf decode: ', gbEncodedCnCh.decode('utf-8', errors='replace'))
+# 你好吗 gb decoded -> utf decode:  �����
+print('你好吗 gb decoded -> gb decode: ', gbEncodedCnCh.decode('gb2312', errors='replace'))
+# 你好吗 gb decoded -> gb decode:  你好吗
+print('你好吗 utf encoded -> gb decode: ', utfEncodedCnCh.decode('gb2312', errors='replace'))
+# 你好吗 utf encoded -> gb decode:  浣�濂藉��
 
 ## example 4-6
 city = 'São Paulo'
