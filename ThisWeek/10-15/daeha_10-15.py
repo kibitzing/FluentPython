aa34a95cac574fe88d02b33be0c002362e6b89f2
seu5022 <kdhht5022@gmail.com>
2018-10-15 23:50:20 +0900

    daeha upload code of 10-15
    daeha upload code of 10-15

---
+++ ThisWeek/10-15/daeha_10-15.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "daehakim"
+__email__ = "kdhht5022@gmail.com"
+
+"""
+    string을 clipping하는 에제
+"""
+
+def clip(text, max_len=100):
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+            if space_after >= 0:
+                end = space_after
+
+    if end is None:
+        end = len(text)
+
+    return text[:end].rstrip()
+
+clip.__defaults__
+
+clip.__code__
+
+clip.__code__.co_varnames
+
+clip.__code__.co_argcount
+
+
