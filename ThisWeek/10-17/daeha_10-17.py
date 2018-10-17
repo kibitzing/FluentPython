06c5bf0c8f2e31ab4c712e3d210036249c4ae6a3
seu5022 <kdhht5022@gmail.com>
2018-10-17 23:37:02 +0900

    daeha upload on 10-17
    daeha upload on 10-17

---
+++ ThisWeek/10-17/daeha_10-17.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "daehakim"
+__email__ = "kdhht5022@gmail.com"
+
+"""
+    파이썬 추상 클래스를 사용하는 예제
+    파이썬은 추상 클래스라는 기능을 제공함
+    추상 클래스는 메서드의 목록만 가진 클래스이며 상속받는 클래스에서 메서드 구현을 강제하기 위해 사용함
+"""
+
+from abc import *
+
+class StudentBase(metaclass=ABCMeta):
+    @abstractmethod
+    def study(self):
+        pass
+ 
+    @abstractmethod
+    def go_to_school(self):
+        pass
+    
+    
+class Student(StudentBase):
+    def study(self):
+        print('열공하기!')
+        
+dd = Student()
+dd.study()  
+
+# TypeError: Can't instantiate abstract class Student with abstract methods go_to_school
+# StudentBase를 상속받은 Student에서는 study 메서드만 구현하고, 
+# go_to_school 메서드는 구현하지 않았으므로 에러가 발생
+
+class Student(StudentBase):
+    def study(self):
+        print('열공하기!')
+ 
+    def go_to_school(self):
+        print('늦잠잔 뒤에 학교 천천히 가기')
+        
+dd = Student()
+dd.study()
+dd.go_to_school()
+
+# 예제와 같이 추상 클래스는 파생 클래스가 반드시 구현해야 하는 메서드를 정해줄 수 있음
+
+
