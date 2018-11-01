de185f3d9d613feb9a20ab785381631c4df62e60
seu5022 <kdhht5022@gmail.com>
2018-11-01 19:35:56 +0900

    daeha upload code of 11-01
    daeha upload code of 11-01

---
+++ ThisWeek/11-01/daeha_11-01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Working example for my blog post at:
+# https://danijar.github.io/structuring-your-tensorflow-models
+
+""" WeakValueDictionary에 관한 예제
+"""
+
+class Cheese:
+    
+    def __init__(self, kind):
+        self.kind = kind
+        
+    def __repr__(self):
+        return 'cheese(%r)' % self.kind
+    
+    
+import weakref
+stock = weakref.WeakValueDictionary()
+
+bag = [Cheese('Red Leicester'), Cheese('Parmasan'),
+       Cheese('Tilsit'), Cheese('Brie')]
+
+
+for cheese in bag:
+    stock[cheese.kind] = cheese
+    
+sorted(stock.keys())  # ['Brie', 'Parmasan', 'Red Leicester', 'Tilsit']
+
+del bag
+sorted(stock.keys())  # ['Brie']
+
+del cheese
+sorted(stock.keys())  # []
