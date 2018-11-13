1fcd4326629ec52f5d7c4f1ddf487ce864455407
ㅋ <zelabean@naver.com>
2018-11-13 19:27:02 +0900

    Seongbin upload 11-13

---
+++ ThisWeek/11-13/Seongbin_11_13.py
@@ -0,0 +1,102 @@
+
+
+## this code is 11_07's code
+from array import array
+import math
+import reprlib
+import numbers
+import functools
+import operator
+import itertools
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes[ord(self.typecode)] +
+        bytes(self._components))
+
+    def __eq__(self, other):
+        return (len(self) == len(other) and all(a == b for a, b in zip(self,other)))
+
+    def __hash__(self):
+        hashes = (hash(x) for x in self)
+        return functools.reduce(operator.xor, hashes, 0)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, index):
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._components[index])
+        elif isinstance(index, numbers.Integral):
+            return self._components[index]
+        else:
+            msg = '{.__name__} indices must be integers'
+            raise TypeError(msg.format(cls))
+
+    shortcut_names = 'xyzt'
+
+    def __getattr__(self, item):
+        cls = type(self)
+        if len(item) == 1:
+            pos = cls.shortcut_names.find(item)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+        msg = '{.__name__!r} object has no attribute {!r}'
+        raise AttributeError(msg.format(cls, item))
+
+    def angle(self, n):
+        r = math.sqrt(sum(x * x for x in self[n:]))
+        a = math.atan2(r, self[n-1])
+        if(n == len(self) -1) and (self[-1] < 0):
+            return math.pi * 2 - a
+        else:
+            return a
+
+    def angles(self):
+        return (self.angle(n) for n in range(1, len(self)))
+
+    def __format__(self, format_spec=' '):
+        if format_spec.endswith('h'):
+            format_spec = format_spec[:-1]
+            coords = itertools.chain([abs(self)],
+                                     self.angles())
+            outer_format = '<{}>'
+
+        else:
+            coords = self
+            outer_format = '({})'
+
+        components = (format(c, format_spec) for c in coords)
+        return outer_format.format(', '.join(components))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
+
+
+
