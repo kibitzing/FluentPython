95331ecb2f9097fd1facb3f4e880f43fb16934be
seu5022 <kdhht5022@gmail.com>
2018-12-13 22:39:59 +0900

    daeha upload code of 12-13
    daeha upload code of 12-13

--- ThisWeek/12-13/daeha_12-13.py
+++ ThisWeek/12-13/daeha_12-13.py
@@ -1,39 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" 등차수열 제너레이터 알아보는 예제
+""" Tensorflow generator example
+    I'm refer below link:
+    https://gist.github.com/elgehelge/6c7cd65bd08b71b898fb54eb13ed6f98
 
 """
-class ArithmeticCalculation:
+
+import tensorflow as tf
+print('tensorflow version is {}'.format(tf.__version__))
+
+def data_generator(start, end):
+    for x, y in zip(range(start, end), range(start, end)):
+        print(x, y)
+        yield x, y
+        
+
+def input_fn(data_getter):
+    dataset = (tf.data.Dataset.from_generator(
+            generator=lambda: data_getter,
+            output_types=(tf.float32),
+            )
+    .repeat()
+    .make_one_shot_iterator().get_next()
+    )
     
-    def __init__(self, begin, step, end=None):
-        self.begin = begin
-        self.step = step
-        self.end = end
-        
-    def __iter__(self):
-        result = type(self.begin + self.step)(self.begin)
-        forever = self.end is None
-        index = 0
-        
-        while forever or result < self.end:
-            yield result
-            index += 1
-            result = self.begin + self.step * index
-            
-
-ac = ArithmeticCalculation(0, 1, 3)
-list(ac)  # [0, 1, 2]
-
-ac = ArithmeticCalculation(1, .5, 3)
-list(ac)  # [1.0, 1.5, 2.0, 2.5]
+    return dataset[0], dataset[1]
 
 
-from fractions import Fraction
-ac = ArithmeticCalculation(0, Fraction(1, 3), 1)
-list(ac)  # [Fraction(0, 1), Fraction(1, 3), Fraction(2, 3)]
+def model_fn(features, labels, mode):
+    var = tf.Variable(0, dtype=tf.float32)
+    prediction = features + var
+    loss = prediction - labels
+    loss.set_shape([])
+    
+    return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=prediction,
+            loss=loss,
+            train_op=tf.contrib.layers.optimize_loss(
+                    loss=loss,
+                    global_step=tf.train.get_global_step(),
+                    optimizer=tf.train.AdamOptimizer,
+                    learning_rate=0.01
+            ),
+    )
 
-
-from decimal import Decimal
-ac = ArithmeticCalculation(0, Decimal('.1'), .3)
-list(ac)  # [Decimal('0'), Decimal('0.1'), Decimal('0.2')]
+         
+def run():
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+    
+    train_data_gen = data_generator(start=0, end=5)
+    eval_data_gen = data_generator(start=100, end=105)
+    
+    estimator = tf.estimator.Estimator(model_fn=model_fn)
+    train_spec = tf.estimator.TrainSpec(
+            input_fn=lambda: input_fn(train_data_gen))
+    eval_spec = tf.estimator.EvalSpec(
+            input_fn=lambda: input_fn(eval_data_gen), start_delay_secs=0)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    
+    
+if __name__ == '__main__':
+    run()
