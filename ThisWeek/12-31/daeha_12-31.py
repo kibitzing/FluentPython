d9660360d4de3473cb59cb87eac79bffc4b37c56
seu5022 <kdhht5022@gmail.com>
2018-12-31 22:28:06 +0900

    daeha upload code of 12-31
    daeha upload code of 12-31

---
+++ ThisWeek/12-31/daeha_12-31.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" concurrent를 사용하는 예제
+
+"""
+import os
+import time
+import sys
+
+import requests
+
+from concurrent import futures
+
+POP20_CC = ('CN IN US ID BR PK NG BD RU JP '
+            'MX PH VN ET EG DE IR TR CD FR').split()
+
+BASE_URL = 'http://flupy.org/data/flags'
+
+DEST_DIR = 'flags/'
+if os.path.exists(DEST_DIR):
+    os.makedirs(DEST_DIR)
+
+MAX_WORKERS = 20
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+        
+        
+def get_flag(cc):
+    url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):
+    print(text, end=' ')
+    sys.stdout.flush()
+    
+    
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = '\n{} flags downloaded in {:.2f}s'
+    print(msg.format(count, elapsed))
+    
+    
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):
+    workers = min(MAX_WORKERS, len(cc_list))
+    with futures.ThreadPoolExecutor(workers) as executor:
+        res = executor.map(download_one, sorted(cc_list))
+        
+    return len(list(res))
+
+
+
+if __name__ == '__main__':
+    main(download_many)
