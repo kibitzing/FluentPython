f290bb14ccae2beed2c4e77b36a1186576f60d07
kibitzing <truebread@naver.com>
2019-01-10 23:58:57 +0900

    jingu upload cod of 01-10

---
+++ ThisWeek/01-10/jingu_01-10.py
@@ -0,0 +1,75 @@
+#!/anaconda3/envs/tensorflow/bin/python
+#-*- coding: utf-8 -*-
+"""
+    Created by Jingu Kang on 10/01/2019.
+    Copyright © 2018 Jingu Kang. All rights reserved.
+
+    DESCRIPTION:
+      download flags.
+      modified get_flag(cc)
+"""
+import os
+import sys
+import time
+import asyncio
+import aiohttp
+import requests
+
+POP20_CC = ('CN IN US ID BR PK NG BD RU JP '
+            'MX PH VN ET EG DE IR TR CD FR').split()
+
+BASE_URL = 'http://flupy.org/data/flags'
+
+DEST_DIR = './downloads/'
+
+if not os.path.exists(DEST_DIR):
+    os.mkdir(DEST_DIR)
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+
+def get_flag(cc):
+    url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+def show(text):
+    print(text, end=' ')
+    sys.stdout.flush()
+
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = '\n{} flags downloaded in {:.2}s '
+    print(msg.format(count, elapsed))
+
+@asyncio.coroutine
+def get_flag(cc):
+    url='{}/{cc}.jpg'.format(BASE_URL,cc=cc)
+    print(url)
+    resp = yield from aiohttp.ClientSession().get(url)
+    print (resp.status)
+    image = yield from resp.read()
+    return image
+
+@asyncio.coroutine
+def download_one(cc):
+    image = yield from get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+def download_many(cc_list):
+    loop = asyncio.get_event_loop()
+    to_do = [download_one(cc) for cc in sorted(cc_list)]
+    wait_coro = asyncio.wait(to_do)
+    res, _ = loop.run_until_complete(wait_coro)
+    loop.close()
+
+    return len(res)
+
+if __name__ == '__main__':
+    main(download_many)
