24c12c5c3c33c4625684a36791c0856ac6b2f8a4
ㅋ <zelabean@naver.com>
2019-01-14 20:37:03 +0900

    seongbin update 01-14

---
+++ ThisWeek/01-14/seongbin_01_14.py
@@ -0,0 +1,41 @@
+from collections import abc
+import keyword
+class FrozenJSON:
+
+    def __init__(self, mapping):
+        self.__data = {}
+        for key, value in mapping.items():
+            if keyword.iskeyword(key):
+                key += '_'
+            self.__data[key] = value
+
+    def __getattr__(self, name):
+        if hasattr(self.__data, name):
+            return getattr(self.__data, name)
+        else:
+            return FrozenJSON.build(self.__data[name])
+
+
+
+    @classmethod
+    def build(cls, obj):
+        if isinstance(obj, abc.Mapping):
+            return cls(obj)
+        elif isinstance(obj, abc.MutableSequence):
+            return [cls.build(item) for item in obj]
+        else:
+            return obj
+
+grad = FrozenJSON({'name':'Jim Bo','class':1982})
+
+#print(grad.class)
+
+print(getattr(grad,'class'))
+#print(grad.class_)
+x = FrozenJSON({'2be':'or not'})
+#print(x.2be)
+def object_maker(the_class, some_arg):
+    new_object = the_class.__new__(some_arg)
+    if isinstance(new_object, the_class):
+        the_class.__init__(new_object, some_arg)
+    return new_object
