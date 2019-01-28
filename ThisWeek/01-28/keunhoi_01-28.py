5c45fefa16113b2e80692355c9424b113fed35c9
khahn0213 <31444829+khahn0213@users.noreply.github.com>
2019-01-28 21:37:15 +0900

    Update keunhoi_01-28.py

--- ThisWeek/01-28/keunhoi_01-28.py
+++ ThisWeek/01-28/keunhoi_01-28.py
@@ -9,9 +9,9 @@
 
 # Example 20-2
 class Quantity:
-  __counter = 0
+    __counter = 0
 
-	def __init__(self):
+    def __init__(self):
         cls = self.__class__
         prefix = cls.__name__
         index = cls.__counter
@@ -19,25 +19,25 @@ class Quantity:
         cls.__counter += 1
 
     def __get__(self, instance, owner):
-        return getattr(instance, self.storage_name)
+	return getattr(instance, self.storage_name)
 
-	def __set__(self, instance, value):
-		if value > 0:
+    def __set__(self, instance, value):
+        if value > 0:
             setattr(instance, self.storage_name, value)
-		else:
-			raise ValueError('value must be > 0')
+        else:
+            raise ValueError('value must be > 0')
 
 class LineItem:
-	weight = Quantity()
-	price = Quantity()
+    weight = Quantity()
+    price = Quantity()
 
-	def __init__(self, description, weight, price):
-		self.description = description
-		self.weight = weight
-		self.price = price
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+	self.price = price
 
-	def subtotal(self):
-		return self.weight * self.price
+    def subtotal(self):
+        return self.weight * self.price
 
 
 # if __name__ == "__main__":
